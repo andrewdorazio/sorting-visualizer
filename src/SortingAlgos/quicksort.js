2672d1ae4ca58092476d67d6fceb5f85b07baf61
AndrewDorazio <adorazio1997@gmail.com>
2022-04-30 18:30:08 -0400

    Second pointer didnt work, still takes pressing the button twice for full sort. Moving onto heap sort and will come back

--- src/SortingAlgos/quicksort.js
+++ src/SortingAlgos/quicksort.js
@@ -29,18 +29,17 @@ const quickSort = (array, low, high, pivotAnimations, lengthAnimations, pivotCha
 
 const partition = (array, low, high, pivotAnimations, lengthAnimations, pivotChange) => {
     // Choose rightmost value as pivot
-    let pivot = low;
-    let left = low + 1;
-    let right = high;
+    let rightPivot = high;
+    let leftPivot = low;
     pivotChange.pop()
     pivotChange.pop()
     pivotChange.push(true, true)
-  //  pivotAnimations.push(high)
     // I is left pointer and starts at -1 so it doesnt skip the first element when swapping
     let i = low - 1;
+ //   let y = high + 1;
     // Loop through array, skipping numbers larger than pivot and swapping smaller numbers with the skipped larger 
     for (let j = low; j <= high-1; j++) {
-        if (array[j] <= pivot) {
+        if (array[j] <= array[rightPivot]) {
             i++;
             [array[i], array[j]] = [array[j], array[i]];
             lengthAnimations.push(i, j)
@@ -58,11 +57,32 @@ const partition = (array, low, high, pivotAnimations, lengthAnimations, pivotCha
             pivotChange.push(false, false)
         }
     }
+/*    for (let z = high; z >= low + 1; z--) {
+        if (array[z] >= array[leftPivot]) {
+            y--;
+            [array[y], array[z]] = [array[z], array[y]];
+            lengthAnimations.push(y, z)
+            pivotChange.push(false, false)
+            lengthAnimations.push(y, z)
+            pivotChange.push(false, false)
+            lengthAnimations.push(array[y], array[z])
+            pivotChange.push(false, false)
+        } else {
+            lengthAnimations.push(y, z);
+            lengthAnimations.push(y, z);
+            lengthAnimations.push(null, null);
+            pivotChange.push(false, false)
+            pivotChange.push(false, false)
+            pivotChange.push(false, false)
+        }
+    } */
     // place pivot point used in partition in the correct spot in array
     [array[i + 1], array[high]] = [array[high], array[i + 1]];
+  //  [array[y - 1], array[low]] = [array[low], array[y - 1]]
 
     // return the pivot point 
     pivotAnimations.push(i + 1, array[i + 1], high, array[high])
+  //  pivotAnimations.push(y - 1, array[y - 1], low, array[low])
     return i + 1
 }
 
